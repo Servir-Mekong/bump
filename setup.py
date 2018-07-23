6b20da7f9b3c58b751745f88bf390be45674bf8a
kmarkert <kel.markert@gmail.com>
2018-07-24 03:24:29 +0700

    the beginnings of a python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='bump',
+      version='0.1',
+      description='Basic Utility Mapping Preprocessor',
+      url='http://github.com/Servir-Mekong/bump',
+      author='Kel Markert',
+      author_email='kel.markert@nasa.gov',
+      license='MIT',
+      packages=['bump'],
+      zip_safe=False)
