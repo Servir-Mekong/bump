7f3ecc65f052591ca3cfd209b9642bff86d3e5be
Ate <poortinga.ate@gmail.com>
2018-07-23 16:00:03 -0500

    added the environment

--- bump/env.py
+++ bump/env.py
@@ -1,1 +1,20 @@
 # main environment package for bump
+
+
+class environment(object):
+	
+	def __init__(self):
+		
+		"""Initialize the environment."""   
+		# Initialize the Earth Engine object, using the authentication credentials.            
+		ee.Initialize()
+		
+		# set dates
+        self.startYear = 2015
+        self.endYear = 2015
+        self.startJulian = 10
+        self.endJulian = 50
+        
+        # define study area
+        self.studyArea = [[103.876,18.552],[105.806,18.552],[105.806,19.999],[103.876,19.999],[103.876,18.552]]
+	
