d5e1431906b263351b5c1c3ffebeb8435581db2c
kmarkert <kel.markert@gmail.com>
2018-07-24 09:17:17 -0500

    added updates to the setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='bump',
-      version='0.1',
+      version='0.0.1',
       description='Basic Utility Mapping Preprocessor',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/Servir-Mekong/bump',
+      packages=setuptools.find_packages(),
       author='Kel Markert',
       author_email='kel.markert@nasa.gov',
       license='MIT',
