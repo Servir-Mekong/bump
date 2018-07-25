5214759a640e78554ef0c710ab8f031394f372b6
kmarkert <kel.markert@gmail.com>
2018-07-25 14:35:18 -0500

    added example credentials file and account variable to env for fetching data from login enabled FTP services

--- bump/env.py
+++ bump/env.py
@@ -3,31 +3,35 @@
 from __future__ import division, print_function
 
 import ee
+import netrc
 
 class environment(object):
 
 	def __init__(self):
 
-		"""Initialize the environment."""
-		# Initialize the Earth Engine object, using the authentication credentials.
-		ee.Initialize()
+        """Initialize the environment."""
+        # Initialize the Earth Engine object, using the authentication credentials.
+        ee.Initialize()
+        
+        self.acct = netrc.netrc('../mycredentials.netrc')
+        
+        
+        self.crs = 'epsg:4326'
+        
+        # set dates
+        self.startYear = 2015
+        self.endYear = 2015
+        self.startJulian = 10
+        self.endJulian = 50
+        
+        self.north = 19.999
+        self.south = 18.552
+        self.east = 105.806
+        self.west = 103.876
+        
+        # define study area
+        self.studyArea = [[self.west,self.south],[self.east,self.south],
+        				   [self.east,self.north],[self.west,self.north],
+   					   [self.west,self.south]]
+        return
 
-		self.crs = 'epsg:4326'
-
-		# set dates
-		self.startYear = 2015
-		self.endYear = 2015
-		self.startJulian = 10
-		self.endJulian = 50
-
-		self.north = 19.999
-		self.south = 18.552
-		self.east = 105.806
-		self.west = 103.876
-
-		# define study area
-		self.studyArea = [[self.west,self.south],[self.east,self.south],
-						  [self.east,self.north],[self.west,self.north],
-						  [self.west,self.south]]
-
-		return
