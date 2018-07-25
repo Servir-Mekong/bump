9a2d8b42758c9327688480a66c4ca47ba1750e48
kmarkert <kel.markert@gmail.com>
2018-07-25 14:14:05 -0500

    defining imports for atms class

--- bump/atms.py
+++ bump/atms.py
@@ -1,1 +1,14 @@
 # ATMS package
+
+from __future__ import division, print_function
+
+import datetime
+import numpy as np
+from osgeo import gdal
+from scipy import ndimage
+
+
+import core
+import env
+
+bumper = env.environment()
