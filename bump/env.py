86305a42a50bf35bef9bcbd4f6d506706fb55cfb
kmarkert <kel.markert@gmail.com>
2018-07-25 14:08:31 -0500

    updated env variables for explicit bounding box

--- bump/env.py
+++ bump/env.py
@@ -1,20 +1,33 @@
 # main environment package for bump
 
+from __future__ import division, print_function
+
+import ee
 
 class environment(object):
-	
+
 	def __init__(self):
-		
-		"""Initialize the environment."""   
-		# Initialize the Earth Engine object, using the authentication credentials.            
+
+		"""Initialize the environment."""
+		# Initialize the Earth Engine object, using the authentication credentials.
 		ee.Initialize()
-		
+
+		self.crs = 'epsg:4326'
+
 		# set dates
-        self.startYear = 2015
-        self.endYear = 2015
-        self.startJulian = 10
-        self.endJulian = 50
-        
-        # define study area
-        self.studyArea = [[103.876,18.552],[105.806,18.552],[105.806,19.999],[103.876,19.999],[103.876,18.552]]
-	
+		self.startYear = 2015
+		self.endYear = 2015
+		self.startJulian = 10
+		self.endJulian = 50
+
+		self.north = 19.999
+		self.south = 18.552
+		self.east = 105.806
+		self.west = 103.876
+
+		# define study area
+		self.studyArea = [[self.west,self.south],[self.east,self.south],
+						  [self.east,self.north],[self.west,self.north],
+						  [self.west,self.south]]
+
+		return
