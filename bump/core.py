5d687c2b13ccee79bec32d72c001c1c8aa8ec4eb
kmarkert <kel.markert@gmail.com>
2018-07-25 14:07:59 -0500

    updated core grid, raster, and collection classes for harmonizing data

--- bump/core.py
+++ bump/core.py
@@ -4,12 +4,16 @@ from __future__ import division, print_function
 
 import glob
 import copy
+import math
 
 import numpy as np
 import xarray as xr
+from pyproj import Proj, transform
+from scipy import interpolate 
 
 import env
 import utils
+#import viirs
 
 
 bumper = env.environment()
@@ -19,8 +23,8 @@ class grid(object):
     def __init__(self,resolution):
 
         if '4326' in bumper.crs:
-            midPoint = [np.mean(bumper.north,bumper.south),
-                        np.mean(bumper.east,bumper.west)]
+            midPoint = [np.mean([bumper.north,bumper.south]),
+                        np.mean([bumper.east,bumper.west])]
             ySpacing, xSpacing = utils.meters2dd(midPoint,resolution)
 
         elif type(resolution) == list:
@@ -36,16 +40,56 @@ class grid(object):
         self.xx,self.yy = np.meshgrid(lons,lats)
 
         self.nominalResolution = [xSpacing,ySpacing]
+        self.dims = self.xx.shape
 
         return
+    
+    def mapRaster(self,raster,interpMethod='linear'):
+        
+        rasterLons = raster.coords['lon']
+        rasterLats = raster.coords['lat']
+        
+        
+        xSelect = (rasterLons>bumper.west) & (rasterLons<bumper.east)
+        ySelect = (rasterLats>bumper.south) & (rasterLats<bumper.north)
+        spatialSelect = ySelect & xSelect
+        
+        idx = np.where(spatialSelect == True)
+        
+        # Format geolocation coordinates for gridding
+        pts = np.zeros((idx[0].size,2))
+        pts[:,0] = rasterLons[idx].ravel()
+        pts[:,1] = rasterLats[idx].ravel()
+        
+        bNames = raster.bands.keys()
+                
+        out = raster._copy()
+        
+        for i in range(len(bNames)):
+            if 'mask' in bNames[i]:
+                iMethod = 'nearest'
+            else:
+                iMethod = interpMethod
+            
+            # Regrid data to common grid 
+            out.bands[bNames[i]] = interpolate.griddata(pts, raster.bands[bNames[i]][idx].ravel(),
+                                           (self.xx,self.yy), method=iMethod)
+            
+            if i == 0:
+                interpMask = np.isnan(out.bands[bNames[i]])
+            
+                
+        out.bands['mask'] = out.bands['mask'] & ~interpMask
+        out.updateMask()
+            
+        out.coords['Lon'],out.coords['Lat'] = self.xx,self.yy
+        out.extent = bumper.west,bumper.south,bumper.east,bumper.north
 
-    def raster2grid(self,raster):
-
-        return
+        return out
 
 
 class raster(object):
-    def __init__(self,path,sensor,crs='epsg:4326'):
+    def __init__(self,path,sensor):
         self.src = path
         self.sensor = sensor
         self.crs = {'init':bumper.crs}
@@ -78,9 +122,137 @@ class raster(object):
 
         return out
 
+    def _geoGrid(self,extent,dims,nativeProj=None,wgsBounds=True):
+
+        west, south, east, north = extent
+
+        gcsProj = Proj(init=bumper.crs)
+        native = Proj(nativeProj)
+
+        if wgsBounds == False:
+            native = Proj(nativeProj)
+
+            llx,lly = transform(gcsProj,native,west,south)
+            urx,ury = transform(gcsProj,native,east,north)
+
+            yCoords = np.linspace(lly,ury,dims[0],endpoint=False)[::-1]
+            xCoords = np.linspace(llx,urx,dims[1],endpoint=False)
+
+            xx,yy = np.meshgrid(xCoords,yCoords)
+
+            lons,lats = transform(native,gcsProj,xx,yy)
+
+        elif wgsBounds == True:
+            llx,lly = west,south
+            urx,ury = east,north
+
+            yCoords = np.linspace(lly,ury,dims[0],endpoint=False)[::-1]
+            xCoords = np.linspace(llx,urx,dims[1],endpoint=False)
+
+            lons,lats = np.meshgrid(xCoords,yCoords)
+
+        else:
+            raise ValueError('argument "wgsBounds" needs to be binary')
+
+        return lons,lats
+    
+    def _getGt(self,north,west,pxSize,projStr=None):
+        if projStr:
+            outProj = Proj(projStr)
+            inProj = Proj(init=bumper.crs)
+
+        ulx,uly = transform(inProj,outProj,west,north)
+
+        #(originX, pixelWidth, 0, originY, 0, pixelHeight)
+
+        gt = (ulx,pxSize,0,uly,0,-pxSize)
+
+        return gt
+    
+    def updateMask(self):
+        bNames = [i for i in self.bands.keys() if i != 'mask']
+        mask = self.bands['mask']
+                
+        for i in bNames:
+            data = self.bands[i]
+            self.bands[i] = np.ma.masked_where(mask==0,data)
+            
+        return
+    
+    def unmask(self,value=None):
+        bNames = [i for i in self.bands.keys() if i != 'mask']
+        mask = self.bands['mask']
+        
+        out = self._copy()
+
+        if value:
+            for i in bNames:
+                out.bands[i][np.where(mask==0)] = value
+                
+        else:
+            for i in bNames:
+                out.bands[i] = self.bands[i].data
+                
+        return out
+    
+    def writeGeotiff(self,fileName):
+        
+            return
+        
+    def normalizedDifference(self,band1,band2):
+        nd = (self.bands[band1] - self.bands[band2]) / (self.bands[band1] + self.bands[band2])
+        
+        self.bands['nd'] = nd
+        
+        clone = self._copy()
+
+        for n in clone.bandNames:
+            if n != 'mask':
+                del clone.bands[n]
+            
+        clone.bandNames = ['mask','nd']
+        
+        return clone
+    
+
 
 class collection(object):
-    def __init__(self,folder,grid):
+    def __init__(self,rasterList,gr):
+        bandList = rasterList[0].bandNames
+        collDates = []
+        
+        x = gr.xx[0,:]
+        y = gr.yy[:,0]
+        
+        data = {'x':x,'y':y}
+        
+        for i in range(len(bandList)):
+            tData = np.zeros([gr.dims[0],gr.dims[1],len(rasterList)])
+            for j in range(len(rasterList)):
+                if rasterList[j].bandNames != bandList:
+                    raise AttributeError('All band names for rasters must be the same for collection')
+                        
+                else:
+                    if i == 0:
+                        collDates.append(rasterList[j].coords['date'])
+                    temp = rasterList[j].unmask(-9999)
+                    tData[:,:,j] = np.flipud(temp.bands[bandList[i]])
+                    
+                data[bandList[i]] = (['y','x','time'],tData)
+        
+            
+        ds = xr.Dataset(data,
+                coords={'lon': (['y', 'x'], gr.xx),
+                        'lat': (['y', 'x'], gr.yy),
+                        'time': collDates}
+                        )
+        
+        self.data = ds.where(ds!=-9999)
+        
+        return 
+    
+    def writeNetCDF(self,fileName):   
+        
 
 
         return
