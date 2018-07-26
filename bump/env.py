1be23b3ca2d289e4a2cefa56a79671a3562d4cc7
kmarkert <kel.markert@gmail.com>
2018-07-26 17:01:09 -0500

    minor changes to environment variables

--- bump/env.py
+++ bump/env.py
@@ -7,31 +7,40 @@ import netrc
 
 class environment(object):
 
-	def __init__(self):
-
+    def __init__(self):
         """Initialize the environment."""
         # Initialize the Earth Engine object, using the authentication credentials.
         ee.Initialize()
         
         self.acct = netrc.netrc('../mycredentials.netrc')
         
-        
         self.crs = 'epsg:4326'
         
         # set dates
-        self.startYear = 2015
-        self.endYear = 2015
+        self.startYear = 2018
+        self.endYear = 2018
         self.startJulian = 10
         self.endJulian = 50
         
-        self.north = 19.999
-        self.south = 18.552
-        self.east = 105.806
-        self.west = 103.876
+        self.startDate='2018-07-22'
+        self.endDate='2018-07-25'
+        
+        self.north = 17.500
+        self.south = 15.000
+        self.east = 105.000
+        self.west = 102.500
         
         # define study area
         self.studyArea = [[self.west,self.south],[self.east,self.south],
         				   [self.east,self.north],[self.west,self.north],
    					   [self.west,self.south]]
+        
+        
+        self.viirs = True
+        self.atms = False
+        self.landsat = False
+        self.sentinel1 = False
+        self.sentinel2 = False
+        
         return
 
