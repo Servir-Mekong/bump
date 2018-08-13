8bc2935006ec65ef09a05c512ec046b239cd2b0a
Ate <poortinga.ate@gmail.com>
2018-08-13 16:07:14 +0700

    adjusted terrain correction

--- bump/sentinel2.py
+++ bump/sentinel2.py
@@ -455,6 +455,33 @@ class functions():
 			
 			return ee.Image(img_plus_ic);
  
+			# Calculate the Illumination Condition (IC)
+			# slope part of the illumination condition
+			cosZ = SZ_rad.cos();
+			cosS = slp_rad.cos();
+			slope_illumination = cosS.expression("cosZ * cosS", \
+												{'cosZ': cosZ, 'cosS': cosS.select('slope')});
+			
+			
+			# aspect part of the illumination condition
+			sinZ = SZ_rad.sin(); 
+			sinS = slp_rad.sin();
+			cosAziDiff = (SA_rad.subtract(asp_rad)).cos();
+			aspect_illumination = sinZ.expression("sinZ * sinS * cosAziDiff", \
+                                           {'sinZ': sinZ, \
+                                            'sinS': sinS, \
+                                            'cosAziDiff': cosAziDiff});
+			
+			# full illumination condition (IC)
+			ic = slope_illumination.add(aspect_illumination);
+			
+			
+
+			# Add IC to original image
+			img_plus_ic = ee.Image(img.addBands(ic.rename(['IC'])).addBands(cosZ.rename(['cosZ'])).addBands(cosS.rename(['cosS'])).addBands(slp.rename(['slope'])));
+			
+			return ee.Image(img_plus_ic);
+ 
 		def topoCorr_SCSc(img):
 			img_plus_ic = img;
 			mask1 = img_plus_ic.select('nir').gt(-0.1);
@@ -464,14 +491,14 @@ class functions():
 
 			img_plus_ic_mask2 = ee.Image(img_plus_ic.updateMask(mask2));
 
-			bandList = ee.List(['cb','blue','green','red']) #,'re1','re2','re3','nir','nir2','waterVapor','cirrus','swir1','swir2']); # Specify Bands to topographically correct
+			bandList = ee.List(['cb','blue','green','red','re1','re2','re3','nir','nir2','waterVapor','cirrus','swir1','swir2']); # Specify Bands to topographically correct
     
 			def apply_SCSccorr(band):
 				method = 'SCSc';
 			
 				out = img_plus_ic_mask2.select('IC', band).reduceRegion(reducer= ee.Reducer.linearFit(), \
-																			geometry= img.geometry().buffer(-5000), \
-																			scale= 10, \
+																			geometry= ee.Geometry(img.geometry().buffer(-5000)), \
+																			scale= 30, \
 																			maxPixels = 1e13); 
 
 				out_a = ee.Number(out.get('scale'));
@@ -490,21 +517,12 @@ class functions():
 			
 				
 			# need to fix this in to map.. 
-			img_SCSccorr = img.select([]).addBands(apply_SCSccorr("cb")) \
-										 .addBands(apply_SCSccorr("blue")) \
-										 .addBands(apply_SCSccorr("green")) \
-										 .addBands(apply_SCSccorr("red"))\
-								#		 .addBands(apply_SCSccorr("re1")) \
-								#		 .addBands(apply_SCSccorr("re2"))\
-								#		 .addBands(apply_SCSccorr("re3"))\
-								#		 .addBands(apply_SCSccorr("nir"))\
-								#		 .addBands(apply_SCSccorr("nir2"))\
-								#		 .addBands(apply_SCSccorr("waterVapor"))\
-								#		 .addBands(apply_SCSccorr("cirrus"))\
-								#		 .addBands(apply_SCSccorr("swir1"))\
-								#		 .addBands(apply_SCSccorr("swir2"))\
-	
-	
+			img_SCSccorr = img.select([]).addBands(apply_SCSccorr("blue")).addBands(apply_SCSccorr("red")) \
+																		  .addBands(apply_SCSccorr("green"))\
+																		  .addBands(apply_SCSccorr("nir")) \
+																		  .addBands(apply_SCSccorr("swir1"))\
+																		  .addBands(apply_SCSccorr("swir2"))\
+																		  
 			return img_SCSccorr.unmask(img_plus_ic.select(bandList)) 
 	
 		
@@ -513,6 +531,83 @@ class functions():
 		img = topoCorr_SCSc(img)
 		
 		return img
+  	
+ 
+	def brdf(self,img):   
+		
+		import sun_angles
+		import view_angles
+
+	
+		def _apply(image, kvol, kvol0):
+			blue = _correct_band(image, 'blue', kvol, kvol0, f_iso=0.0774, f_geo=0.0079, f_vol=0.0372)
+			green = _correct_band(image, 'green', kvol, kvol0, f_iso=0.1306, f_geo=0.0178, f_vol=0.0580)
+			red = _correct_band(image, 'red', kvol, kvol0, f_iso=0.1690, f_geo=0.0227, f_vol=0.0574)
+			nir = _correct_band(image, 'nir', kvol, kvol0, f_iso=0.3093, f_geo=0.0330, f_vol=0.1535)
+			swir1 = _correct_band(image, 'swir1', kvol, kvol0, f_iso=0.3430, f_geo=0.0453, f_vol=0.1154)
+			swir2 = _correct_band(image, 'swir2', kvol, kvol0, f_iso=0.2658, f_geo=0.0387, f_vol=0.0639)
+			return replace_bands(image, [blue, green, red, nir, swir1, swir2])
+
+
+		def _correct_band(image, band_name, kvol, kvol0, f_iso, f_geo, f_vol):
+			"""fiso + fvol * kvol + fgeo * kgeo"""
+			iso = ee.Image(f_iso)
+			geo = ee.Image(f_geo)
+			vol = ee.Image(f_vol)
+			pred = vol.multiply(kvol).add(geo.multiply(kvol)).add(iso).rename(['pred'])
+			pred0 = vol.multiply(kvol0).add(geo.multiply(kvol0)).add(iso).rename(['pred0'])
+			cfac = pred0.divide(pred).rename(['cfac'])
+			corr = image.select(band_name).multiply(cfac).rename([band_name])
+			return corr
+
+
+		def _kvol(sunAz, sunZen, viewAz, viewZen):
+			"""Calculate kvol kernel.
+			From Lucht et al. 2000
+			Phase angle = cos(solar zenith) cos(view zenith) + sin(solar zenith) sin(view zenith) cos(relative azimuth)"""
+			
+			relative_azimuth = sunAz.subtract(viewAz).rename(['relAz'])
+			pa1 = viewZen.cos() \
+				.multiply(sunZen.cos())
+			pa2 = viewZen.sin() \
+				.multiply(sunZen.sin()) \
+				.multiply(relative_azimuth.cos())
+			phase_angle1 = pa1.add(pa2)
+			phase_angle = phase_angle1.acos()
+			p1 = ee.Image(PI().divide(2)).subtract(phase_angle)
+			p2 = p1.multiply(phase_angle1)
+			p3 = p2.add(phase_angle.sin())
+			p4 = sunZen.cos().add(viewZen.cos())
+			p5 = ee.Image(PI().divide(4))
+
+			kvol = p3.divide(p4).subtract(p5).rename(['kvol'])
+
+			viewZen0 = ee.Image(0)
+			pa10 = viewZen0.cos() \
+				.multiply(sunZen.cos())
+			pa20 = viewZen0.sin() \
+				.multiply(sunZen.sin()) \
+				.multiply(relative_azimuth.cos())
+			phase_angle10 = pa10.add(pa20)
+			phase_angle0 = phase_angle10.acos()
+			p10 = ee.Image(PI().divide(2)).subtract(phase_angle0)
+			p20 = p10.multiply(phase_angle10)
+			p30 = p20.add(phase_angle0.sin())
+			p40 = sunZen.cos().add(viewZen0.cos())
+			p50 = ee.Image(PI().divide(4))
+
+			kvol0 = p30.divide(p40).subtract(p50).rename(['kvol0'])
+
+			return (kvol, kvol0)
+         
+ 		date = img.date()
+		footprint = determine_footprint(img)
+		(sunAz, sunZen) = sun_angles.create(date, footprint)
+		(viewAz, viewZen) = view_angles.create(footprint)
+		(kvol, kvol0) = _kvol(sunAz, sunZen, viewAz, viewZen)
+		return _apply(img, kvol.multiply(PI()), kvol0.multiply(PI()))
+
+
 
      		
 if __name__ == "__main__":        
